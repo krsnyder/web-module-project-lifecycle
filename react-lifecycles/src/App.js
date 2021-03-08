8e045e89269128ec0308f3e6271963b2e7ae8ee7
Kirk <kirkrobertsnyder@gmail.com>
2021-03-08 18:40:58 -0500

    Made get request when component mounts and pass response data down to UserCard component

--- react-lifecycles/src/App.js
+++ react-lifecycles/src/App.js
@@ -1,11 +1,36 @@
+import React from 'react'
+import axios from 'axios'
+import UserCard from './components/UserCard'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  state = {
+    userInfo: []
+  }
 
-    </div>
-  );
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/krsnyder`)
+      .then(res => {
+        this.setState({
+          userInfo: res.data
+        })
+      })
+      .then(() => console.log(this.state.userInfo))
+      .catch(err => console.log(err))
+  }
+
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Module 2 Project</h1>
+        <UserCard userDetails={this.state.userInfo} />
+      </div>
+    )
+}
 }
 
 export default App;
---
+++ react-lifecycles/src/components/UserCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function UserCard(props) {
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
